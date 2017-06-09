c340593ba61f2acb39ca81b79ceaa98f5afa6dec
Pankaj <pks3kor@gmail.com>
2017-06-09 14:27:33 +0530

    Hello world and some more inbuilt printing
    Hello world and some more inbuilt printing

---
+++ Learn_Python/Hello_World.py
@@ -0,0 +1,14 @@
+"""
+Author = Pankaj Soni
+"""
+a = 2
+b = 3
+print "Hello_World" # print anything
+
+print locals() # print all local definedlocal variables
+print "\n"
+print globals() # print all global definedlocal variables
+print "\n"
+print __file__ # Print script file name
+
+print __doc__ # Print documentation part start with """ and end with """
